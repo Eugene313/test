d00c1fb71b5706e6953bb153d9514874ebfc3256
EUGENE <RUBEL.EUGENE@GMAIL.COM>
2019-04-04 18:55:53 +0300

    2.0

--- src/js/app.js
+++ src/js/app.js
@@ -17,3 +17,50 @@ const sliderChange = () => {
 }
 setInterval(sliderChange,5000);
 
+
+const sliderItem = $('.snowboards-item')
+let curentPosition = parseInt($('.wrap').css('left'))
+let sliderIndex = 1
+
+const next = $('#next').click(function(){
+    $('#prev,#next').css({
+        'opacity':'1'
+    })
+    if (sliderIndex < sliderItem.length-1) {
+        $(sliderItem).removeClass('active')
+        $('.wrap').css({
+            'left': (curentPosition -= 400)+'px'
+        })
+        sliderIndex++
+        $(sliderItem[sliderIndex]).addClass('active')
+        console.log(curentPosition)
+    }
+    if (sliderIndex === sliderItem.length-1){
+        $('#next').css({
+            'opacity':'0.2'
+        })
+    }
+});
+
+const prev = $('#prev').click(function(){
+    $('#prev,#next').css({
+        'opacity':'1'
+    })
+    if (sliderIndex > 0) {
+        $(sliderItem).removeClass('active')
+        $('.wrap').css({
+            'left': (curentPosition += 400)+'px'
+        })
+        sliderIndex--
+        $(sliderItem[sliderIndex]).addClass('active')
+        console.log(curentPosition)
+    } 
+    if (sliderIndex === 0){
+        $('#prev').css({
+            'opacity':'0.2'
+        })
+    }
+});
+
+
+
